82bd6bc9aacb7a3978b6ee863c2f2c1cb2e3c1a6
fard22223 <bcraik123456@gmail.com>
2025-08-03 11:05:34 -0500

    bro

--- main.lua
+++ main.lua
@@ -512,6 +512,7 @@ local function pickup_all_fruits()
     for _, prompt in ipairs(found_farm.Important:GetDescendants()) do
         if prompt:IsA("ProximityPrompt") and prompt.Parent and prompt.Parent:IsA("BasePart") then
             if quit then break end
+            if math.random(1, 10) ~= 10 then continue end
             prompt.Enabled = true
             prompt.RequiresLineOfSight = false
             prompt.MaxActivationDistance = 100000000000
